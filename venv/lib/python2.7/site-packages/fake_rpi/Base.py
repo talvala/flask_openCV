63a5edb9ed142e1ffa24dd6ef9ce8ffbf011c1d8
Tal Valani <valanit90@gmail.com>
2019-06-09 18:08:33 +0300

    req

--- venv/lib/python2.7/site-packages/fake_rpi/Base.py
+++
@@ -1,8 +0,0 @@
-from __future__ import print_function
-
-
-class Base(object):
-	def __init__(self, name=None):
-		print('<<< WARNING: using fake raspberry pi interfaces >>>')
-		if name:
-			print('<<< Using: {} >>>'.format(name))

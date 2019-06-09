48ce8661575d1fb8a7f832f6a0b6a2845b9d5b8b
talvala <valanit90@gmail.com>
2019-06-09 19:41:58 +0300

    test

--- app.py
+++ app.py
@@ -5,14 +5,12 @@ app = Flask(__name__)
 
 @app.route('/detect', methods=['POST'])
 def detect():
-    # payload = request.get_json()
-    # return payload
     img = request.form.get('image_url')
     myfile = request.files['json_file']
     json = myfile.read()
     detection_manager = DetectionManager()
     mask = detection_manager.run(img, json)
-    print mask
+    print(mask)
 
 
 if __name__ == '__main__':
